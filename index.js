33c5162a03f07efc4b81ef5301a5e8b7ea1c1390
DevGui39 <guilherme.gomesesteves@gmail.com>
2024-10-11 14:02:40 -0300

    desafio concluido

---
+++ index.js
@@ -0,0 +1,6 @@
+let idadeinserida = parseInt(prompt("insira sua idade"));
+let anoAtual = parseInt(prompt("insira ano atual"));
+
+let AnoDeNascimento = anoAtual - idadeinserida;
+
+alert("esse foi o ano que você nasceu " + AnoDeNascimento);
